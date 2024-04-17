9afddb92a0fc7249073c5983012c7beebf69fff0
jpaliaga10 <155824170+jpaliaga10@users.noreply.github.com>
2024-04-17 16:05:37 +0000

    jpaliaga10-traffick-light

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [selectedColor, setSelectedColor] = useState("yellow");
+
+    const handleColorChange = (color) => {
+        setSelectedColor(color);
+    };
+
+    return (
+        <div className="container">
+            <div className="palo">
+            </div>
+        <div className="traffic-light">
+            <div
+                onClick={() => handleColorChange("red")}
+                className={"light red" + (selectedColor === "red" ? " glow" : "")}
+            ></div>
+            <div
+                onClick={() => handleColorChange("yellow")}
+                className={"light yellow" + (selectedColor === "yellow" ? " glow" : "")}
+            ></div>
+            <div
+                onClick={() => handleColorChange("green")}
+                className={"light green" + (selectedColor === "green" ? " glow" : "")}
+            ></div>
+
+            {/* Botón para alternar el color */}
+            <button onClick={() => handleColorChange("red")}>Rojo</button>
+            <button onClick={() => handleColorChange("yellow")}>Amarillo</button>
+            <button onClick={() => handleColorChange("green")}>Verde</button>
+        </div>
+        </div>
+    );
 };
 
 export default Home;
